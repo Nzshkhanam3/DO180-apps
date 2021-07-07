e4f43a3866def0e981e7d184d62f4a5f50db5f29
Nzshkhanam3 <63281081+Nzshkhanam3@users.noreply.github.com>
2021-07-07 13:58:13 +0530

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,16 +1,3 @@
 <?php
-//Create a bluegreen deployment demo 
-$in = imagecreatetruecolor(200, 200);
-$white = imagecolorallocate($in, 255, 255, 255);
-$blue = imagecolorallocate($in, 0, 0, 255);
-$green = imagecolorallocate($in, 0 , 255, 0);
-
-// Draw a white rectangle 
-// imagefilledrectangle($in, 0, 0, 199, 199, $blue);
-// imagefilledrectangle($in, 0, 0, 199, 199, $green);
-
-//Save the image 
-header('Content-Type: image/png');
-imagePNG($in, './imagefilledrectangle.png');
-imagedestroy($in);
+print "Hello World! php version is " . PHP_VERSION . "\n";
 ?>
